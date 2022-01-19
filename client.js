0a7dd227edd38484d5036ace8ec4262c149a91b1
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-18 21:55:12 -0500

    refactored connect function

---
+++ client.js
@@ -0,0 +1,22 @@
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "172.21.236.116",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () =>{
+    console.log("you are now connected!");
+  });
+
+  conn.on("data", (data) => {
+    console.log('Server say: ', data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "172.21.236.116",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () =>{
-    console.log("you are now connected!");
-  });
-
-  conn.on("data", (data) => {
-    console.log('Server say: ', data);
-  });
-
-  return conn;
-};
-
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
