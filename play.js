3ab16fe281ea75c9bbf4d18ddc76b67eabe8e1ba
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-18 22:25:51 -0500

    added input functionality to play

--- play.js
+++ play.js
@@ -1,7 +1,27 @@
 
 // establishes a connection with the game server
+const { stdin } = require('process');
 const connect = require('./client');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    console.log("disconnecting...");
+    process.exit();
+  }
+};
+
+stdin.on("data", handleUserInput);
+
 
 console.log("Connecting ...");
 connect();
-
+setupInput();
